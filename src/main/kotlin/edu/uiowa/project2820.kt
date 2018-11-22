7c01c3b5a0e2bd41e6d5347dff85c89cfafe7780
Justin Sheldon <JSheldon3488@gmail.com>
2018-11-22 00:25:08 -0600

    Refactored and added dynamic sizing

--- src/main/kotlin/edu/uiowa/project2820.kt
+++ src/main/kotlin/edu/uiowa/project2820.kt
@@ -24,7 +24,7 @@ class TTTengine {
     var winningCombo = arrayListOf<Pair<Int,Int>>()
     val player1 = "X"
     val player2 = "O"
-    var currentPlayer = "X"
+    var currentPlayer = player1
     var gameOver = false
     var winner = "Nobody"
     var moves = 0
@@ -33,25 +33,19 @@ class TTTengine {
     fun makeMove(row: Int, column: Int) {
         if (!gameOver) {
             //Make sure no one is already in this space
-            if (board[row][column] != "E") {
-                return
-            }
-            //Make move and check for winner
-            board[row][column] = currentPlayer
-            checkForWinner()
-            //Switch player turn
-            if (currentPlayer == "X") {
-                currentPlayer = "O"
-            } else {
-                currentPlayer = "X"
-            }
-            moves++
-            //Check for Draw
-            if (moves == 9 && winner == "Nobody") {
-                winner = "Draw"
-                gameOver = true
-            }
-        }
+            if (board[row][column] == "E") {
+                //Make move and check for winner
+                board[row][column] = currentPlayer
+                moves++
+                checkForWinner()
+                //Switch player turn
+                if (currentPlayer == player1) { currentPlayer = player2 }
+                else { currentPlayer = player1 }
+                //Check for Draw
+                if (moves == 9 && winner == "Nobody") {
+                    winner = "Draw"
+                    gameOver = true
+        } } }
     }
 
     fun checkForWinner() {
@@ -61,6 +55,7 @@ class TTTengine {
                 if (board[row][0].equals(board[row][1]) && board[row][1].equals(board[row][2])) {
                     gameOver = true
                     winner = currentPlayer
+                    //Save winning combo for animation purposes
                     winningCombo.add(Pair(row, 0))
                     winningCombo.add(Pair(row, 1))
                     winningCombo.add(Pair(row, 2))
@@ -72,6 +67,7 @@ class TTTengine {
                 if (board[0][column].equals(board[1][column]) && board[1][column].equals(board[2][column])) {
                     gameOver = true
                     winner = currentPlayer
+                    //Save winning combo for animation purposes
                     winningCombo.add(Pair(0, column))
                     winningCombo.add(Pair(1, column))
                     winningCombo.add(Pair(2, column))
@@ -82,6 +78,7 @@ class TTTengine {
             if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2])) {
                 gameOver = true
                 winner = currentPlayer
+                //Save winning combo for animation purposes
                 winningCombo.add(Pair(0, 0))
                 winningCombo.add(Pair(1, 1))
                 winningCombo.add(Pair(2, 2))
@@ -91,6 +88,7 @@ class TTTengine {
             if (board[2][0].equals(board[1][1]) && board[1][1].equals(board[0][2])) {
                 gameOver = true
                 winner = currentPlayer
+                //Save winning combo for animation purposes
                 winningCombo.add(Pair(2, 0))
                 winningCombo.add(Pair(1, 1))
                 winningCombo.add(Pair(0, 2))
@@ -102,6 +100,9 @@ class TTTengine {
 class TTTview: Application(){
     val window = Pane()
     val engine = TTTengine()
+    //Dynamically be able to change sizes by just changing these variables
+    val wide = 600.0
+    val high = 600.0
 
     override fun start(primaryStage: Stage) {
         primaryStage.scene = Scene(createViewBoard())
@@ -110,17 +111,15 @@ class TTTview: Application(){
 
     fun createViewBoard() : Parent {
         //Setup the main window
-        window.setPrefSize(600.0, 600.0)
+        window.setPrefSize(wide, high)
         //Create the TicTacToe squares for the window and initialize board
         for (i in 0..2) {
             for (j in 0..2) {
                 val square = TTTSquare()
                 square.row = i
                 square.column = j
-                square.translateX = j * 200.0
-                square.translateY = i * 200.0
-                square.centerX = j * 200.0 + 100
-                square.centerY = i * 200.0 + 100
+                square.translateX = j * wide/3
+                square.translateY = i * high/3
                 window.children.add(square)
         } }
         return window
@@ -129,37 +128,35 @@ class TTTview: Application(){
     fun playWinAnimation(start: Pair<Int,Int>, end: Pair<Int,Int> ){
         //Create a line and add it to the children on the window so it can be used
         val line = Line()
-        line.startX = start.second.toDouble() * 200.0 + 100
-        line.startY = start.first.toDouble() * 200.0 + 100
-        line.endX = start.second.toDouble() * 200.0 + 100
-        line.endY = start.first.toDouble() * 200.0 + 100
+        line.startX = start.second.toDouble() * wide/3 + wide/3*0.5
+        line.startY = start.first.toDouble() * high/3 + high/3*0.5
+        line.endX = start.second.toDouble() * wide/3 + wide/3*0.5
+        line.endY = start.first.toDouble() * high/3 + high/3*0.5
         line.strokeWidth = 4.0
-        if (engine.winner == "X"){ line.stroke = Color.GREEN }
-        if (engine.winner == "O"){ line.stroke = Color.RED }
+        if (engine.winner == engine.player1){ line.stroke = Color.GREEN }
+        if (engine.winner == engine.player2){ line.stroke = Color.RED }
         window.children.add(line)
 
         //Animation Timeline
         val timeline = Timeline()
         timeline.keyFrames.add(KeyFrame(Duration.seconds(1.0),
-                KeyValue(line.endXProperty(), end.second.toDouble() * 200.0 + 100),
-                KeyValue(line.endYProperty(), end.first.toDouble()* 200.0 + 100)))
+                KeyValue(line.endXProperty(), end.second.toDouble() * wide/3 + wide/3*0.5),
+                KeyValue(line.endYProperty(), end.first.toDouble()* high/3 + high/3*0.5)))
         timeline.play()
     }
 
     inner class TTTSquare : StackPane() {
         var textBox = Text()
-        var centerX: Double = 0.0
-        var centerY: Double = 0.0
         var row: Int = 0
         var column: Int = 0
 
         //List of initial parameters for each Square
         init {
-            val border = Rectangle(200.0, 200.0)
+            val border = Rectangle(wide/3.0, high/3.0)
             border.fill = null
             border.stroke = Color.BLACK
             setAlignment(Pos.CENTER)
-            textBox.font = Font.font(88.0)
+            textBox.font = Font.font(wide/6)
             children.addAll(border, textBox)
             //Sets the call on the Node itself instead of needing X and Y coordinates
             setOnMouseClicked { if (textBox.text.isEmpty() && !engine.gameOver) draw() }
@@ -167,22 +164,20 @@ class TTTview: Application(){
 
         //What needs to happen from the views perspective on clicks
         fun draw() {
-            if (engine.currentPlayer == "X") {
+            if (engine.currentPlayer == engine.player1) {
                 textBox.fill = Color.GREEN
-                textBox.text = "X"
+                textBox.text = engine.player1
                 engine.makeMove(row, column)
                 if (engine.gameOver){
                     playWinAnimation(engine.winningCombo[0], engine.winningCombo[2])
                 }
-                engine.currentPlayer = "O"
             } else {
                 textBox.fill = Color.RED
-                textBox.text = "O"
+                textBox.text = engine.player2
                 engine.makeMove(row, column)
                 if (engine.gameOver){
                     playWinAnimation(engine.winningCombo[0], engine.winningCombo[2])
                 }
-                engine.currentPlayer = "X"
             }
         }
     }
